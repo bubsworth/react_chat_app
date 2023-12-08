47ec8a8da79bf474ae9dd32c563d55b87431fbc5
bubsworth <bubbard96@hotmail.com>
2023-12-08 11:12:07 +0000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+
+const ws = new WebSocket("ws://localhost:3000/cable");
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [guid, setGuid] = useState("");
+  const messagesContainer = document.getElementById("messages");
+
+  ws.onopen = () => {
+    console.log("Connected to websocket server");
+    setGuid(Math.random().toString(36).substring(2, 15));
+
+    ws.send(
+      JSON.stringify({
+        command: "subscribe",
+        idenitifier: JSON.stringify({
+          id: guid,
+          channel: "MessagesChannel",
+        }),
+      })
+    )
+  }
+
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    if (data.type === "ping") return;
+    if (data.type === "welcome") return;
+    if (data.type === "confirm_subscription") return;
+
+    const message = data.message;
+
+    setMessagesAndScrollDown([...messages, message]);
+  }
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    resetScroll();
+  }, [messages]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const body = event.target.message.value;
+    event.target.message.value = "";
+
+    await fetch("http://localhost:3000/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ body }),
+    })
+  };
+
+  const fetchMessages = async () => {
+    const response = await fetchMessages("http://localhost:3000/messages");
+    const data = await response.json();
+    setMessagesAndScrollDown(data);
+  };
+
+  const setMessagesAndScrollDown = (data) => {
+    setMessages(data);
+    if (!messagesContainer) return;
+    resetScroll();
+  }
+
+  const resetScroll = () => {
+    if (!messagesContainer) return;
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  }
+
+  return (
+    <div className="App">
+      <div className="messageHeader">
+        <h1>Messages</h1>
+        <p>Guid: {guid}</p>
+      </div>
+      <div className="messages" id="messages">
+        {messages.map((message) => (
+          <div className="message" key={message.id}>
+            <p>{message.body}</p>
+          </div>
+        ))}
+      </div>
+      <div className="messageForm">
+        <form onSubmit={handleSubmit}>
+          <input className="messageInput" type="text" name="messages"/>
+          <button className="messageButton" type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
